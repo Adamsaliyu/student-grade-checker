b8eddc9cc5f8ac627d7a3034ec9989fb5aa46db2
Muhammad Adam Amir <mathsteacher2003@gmail.com>
2026-05-10 19:11:24 +0500

    building the fronts editing both css and php

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student-grade-checker</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+            <div class="input-box">
+                <h3>Enter your score</h3>
+                <form action="">
+                    <label for="username">Username</label><br>
+                    <input type="text"><br>
+                    <label for="grade">Score</label><br>
+                    <input type="number"><br>
+
+                    <button>Submit</button>
+                </form>
+        </div>
+        <div class="output-box">
+
+        </div>
+    </div>
+
+        </div>
+        
+    </div>
+</body>
+</html>
+
 <?php
     echo"Amir";
 
